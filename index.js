446b912beeb21012b8ade8f84024926e3cb10c48
Katherine Shine <dragonart016@gmail.com>
2026-01-15 12:18:51 -0500

    modified index.js to fill out greet() function

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-function greet () {}
+function greet() {
+    var name = prompt('What is your name?')
+    alert('Hello, ' + name)
+
+    var age = prompt('How old are you?')
+    console.log(parseInt(age))
+
+    /* var birthday = confirm('Did you have a birthday yet this year?')
+    if (birthday = true) {
+        alert('You were born in ' + (new Date().getFullYear() - age))
+    } else {
+        alert('You were born in ' + age)
+    }
+    */
+
+    if (window.confirm('Did you have a birthday yet this year?')) {
+        alert('You were born in ' + (new Date().getFullYear() - age))
+    } else {
+        alert('You were born in ' + (new Date().getFullYear() - age - 1))
+    }
+}
